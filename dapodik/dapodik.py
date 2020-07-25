0a93a3d7359f699d32f0c6ba1e8fb1b80e4c2d2b
hexatester <revolusi147id@gmail.com>
2020-07-26 01:00:32 +0700

    initial dapodik

---
+++ dapodik/dapodik.py
@@ -0,0 +1,27 @@
+from requests import Session
+
+
+class Dapodik(Session):
+    def __init__(self, url="http://localhost:5774/", semester_id='20201'):
+        self._semester_id = semester_id
+        self._url = url
+        self.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'
+        })
+
+    def login(self, username: str, password: str, rememberme=True, semester_id='20201'):
+        data = {
+            'username': username,
+            'password': password,
+        }
+        if rememberme == True or rememberme == 'on':
+            data['rememberme'] = 'on'
+        res1 = self.post(self._url+'login', data)
+        # handle redirect
+        url = res1.url if res1.is_redirect else ''
+        res2 = self.get(self._url+url)
+        return 'Memuat Aplikasi' in res2.text
+
+    def logout(self):
+        res = self.get(self._url+'destauth')
+        return res.status_code == 302
