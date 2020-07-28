286227f15d4e6a38a85966e3f41e4647eb8fa05e
hexatester <revolusi147id@gmail.com>
2020-07-28 17:26:59 +0700

    initial rombongan belajar

---
+++ dapodik/rombongan_belajar/__init__.py
@@ -0,0 +1,1 @@
+from .pembelajaran import Pembelajaran
---
+++ dapodik/rombongan_belajar/pembelajaran.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from dapodik.base import BaseData
+
+
+@dataclass
+class Pembelajaran:
+    rombongan_belajar_id: str
+    status_di_kurikulum_str: str
+    mata_pelajaran_id: int
+    nama_mata_pelajaran: str
+    sk_mengajar: str
+    ptk_terdaftar_id: str
+    tanggal_sk_mengajar: str
+    jam_mengajar_per_minggu: 24
+    induk_pembelajaran_id: str = ""
+    status_di_kurikulum: int = 9
+    semester_id: str = "20201"
+    pembelajaran_id: str = "Admin.model.PembelajaranNew-1"
