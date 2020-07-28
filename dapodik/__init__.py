26f5ac48f88b5451d9e5ca7142f6ea96732ec125
hexatester <revolusi147id@gmail.com>
2020-07-28 17:28:14 +0700

    add kwarg for base domain / url

--- dapodik/__init__.py
+++ dapodik/__init__.py
@@ -1,13 +1,16 @@
 __version__ = '0.1.0'
 from requests import Session
 from dapodik.auth import Auth
+from dapodik.config import BASE_URL
 from dapodik.peserta_didik import BasePesertaDidik
 from dapodik.sekolah import BaseSekolah
 
 
 class Dapodik(Auth, BaseSekolah, BasePesertaDidik):
-    def __init__(self):
+    def __init__(self, url: str = BASE_URL, verify: bool = False):
+        self._url = url
         super(Dapodik, self).__init__()
+        self.verify = verify
         self.headers.update({
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'
         })
