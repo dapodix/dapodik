63d5b6f006b8025b07dca835a04eafa7fabefce3
hexatester <revolusi147id@gmail.com>
2020-08-17 17:35:59 +0700

    fix Pembelajaran

--- dapodik/rombongan_belajar/__init__.py
+++ dapodik/rombongan_belajar/__init__.py
@@ -5,6 +5,7 @@ from .pembelajaran import Pembelajaran
 class BaseRombonganBelajar(BaseDapodik):
     _Pembelajaran: Pembelajaran = None
 
+    @property
     def Pembelajaran(self):
         if self._Pembelajaran:
             return self._Pembelajaran
