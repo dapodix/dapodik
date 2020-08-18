f848c9537139a5ce2eaa0eb3a92c015354d98eac
hexatester <revolusi147id@gmail.com>
2020-08-18 17:11:27 +0700

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='dapodik',
+    version='0.1.0',
+    description='Client & alat bantu aplikasi Dapodik',
+    author='Habib Rohman',
+    author_email="habibrohman@protonmail.com",
+    url='https://github.com/hexatester/dapodik',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["dapodik"],
+    install_requires=[
+        'requests'
+        'openpyxl'
+        'click'
+        'dacite'
+        'cachetools'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dapodik=dapodik.__main__:main'
+        ]
+    }
+)
