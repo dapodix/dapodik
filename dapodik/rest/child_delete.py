d9a20081ce7b56bf48583099eccfdd3d48280c9c
hexatester <revolusi147id@gmail.com>
2020-08-24 08:25:22 +0700

    refactor import

--- dapodik/rest/child_delete.py
+++ dapodik/rest/child_delete.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
+from dapodik import DapodikObject
 from dapodik.utils.decorator import set_meta
 
 
 @set_meta('child_delete_id')
 @dataclass(eq=False)
-class ChildDelete:
+class ChildDelete(DapodikObject):
     child_delete_id: int
     jumlah: int
     nama_table: str
--- dapodik/rest/jenjang_pendidikan.py
+++ dapodik/rest/jenjang_pendidikan.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
-from dapodik.base import DapodikObject
+from dapodik import DapodikObject
 from dapodik.utils.decorator import set_meta
 
 
