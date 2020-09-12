947e46263e9b6457741d79ecb3ceb9554e654e31
hexatester <revolusi147id@gmail.com>
2020-09-12 15:12:16 +0700

    improve set_meta

--- dapodik/utils/decorator.py
+++ dapodik/utils/decorator.py
@@ -1,10 +1,17 @@
 from dapodik import DapodikObject
-from typing import Optional, Type, Callable
+from typing import Optional, Type, TypeVar, Callable
+
+DO = TypeVar('DO', bound=DapodikObject)
 
 
-def set_meta(id: str, default: Optional[str] = None) -> Callable:
-    def deco(cls: Type[DapodikObject]) -> Type[DapodikObject]:
+def set_meta(id: str,
+             default: Optional[str] = None,
+             **kwargs: Type[DapodikObject]) -> Callable:
+    def deco(cls: DO) -> DO:
         cls._id = id
+        for key, val in kwargs.items():
+            prop = property(fget=val.getter, fset=val.setter)
+            setattr(cls, key, prop)
         return cls
 
     return deco
