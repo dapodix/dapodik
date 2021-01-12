053043fb06eb73f8628ddc32f0b49d9946ce97a6
hexatester <revolusi147id@gmail.com>
2021-01-12 10:43:24 +0700

    v0.6.1 - Remove lxml & Update setup.py

--- dapodik/version.py
+++ dapodik/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __dapodik_version__ = "2021.b"
--- tests/test_dapodik.py
+++ tests/test_dapodik.py
@@ -2,5 +2,5 @@ from dapodik import __version__, __dapodik_version__
 
 
 def test_version():
-    assert __version__ == "0.6.0"
+    assert __version__ == "0.6.1"
     assert __dapodik_version__ == "2021.b"
