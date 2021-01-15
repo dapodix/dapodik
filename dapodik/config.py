a329c50ad1bbbee564b07957086ea8386cc9fea6
hexatester <revolusi147id@gmail.com>
2021-01-15 16:58:37 +0700

    Reset!

--- dapodik/config.py
+++
@@ -1,8 +0,0 @@
-BASE_URL = "http://localhost:5774/"
-DOMAIN = "http://localhost:5774"
-
-SEMESTER_ID = "20201"
-
-USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
-)
