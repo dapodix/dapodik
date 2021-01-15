4551639845b5d0f5f923155e637277c20277ddb0
hexatester <revolusi147id@gmail.com>
2021-01-15 16:04:45 +0700

    Update USER_AGENT

--- dapodik/config.py
+++ dapodik/config.py
@@ -4,5 +4,5 @@ DOMAIN = "http://localhost:5774"
 SEMESTER_ID = "20201"
 
 USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
 )
