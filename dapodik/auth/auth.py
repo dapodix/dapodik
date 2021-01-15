72a4d1e4931baae2a7e64e7604acff649b65eee8
hexatester <revolusi147id@gmail.com>
2021-01-15 18:26:26 +0700

    Update BaseAuth

--- dapodik/auth/auth.py
+++ dapodik/auth/auth.py
@@ -11,15 +11,44 @@ class BaseAuth(BaseDapodik):
         super(BaseAuth, self).__init__(*args, **kwargs)
 
     def login(
-        self, username: str, password: str, semester_id: str = "20202"
+        self,
+        username: str,
+        password: str,
+        semester_id: str = "20202",
     ) -> List[Pengguna]:
+        """Login ke dapodik dan mendapatkan daftar Pengguna
+
+        Args:
+            username (str): Email dapodik
+            password (str): Password dapodik
+            semester_id (str, optional): Id semester. Defaults to "20202".
+
+        Raises:
+            PenggunaTidakTerdaftar: Username yang Anda masukkan tidak terdaftar!
+            PasswordSalah: Password yang Anda masukkan salah!
+
+        Returns:
+            List[Pengguna]: list dari Pengguna
+        """
         data = {"username": username, "password": password, "semester_id": semester_id}
-        res = self._post("roleperan", data)
+        res = self._post("roleperan", data, allow_redirects=False)
         if res.url.endswith("#PenggunaTidakTerdaftar"):
             raise PenggunaTidakTerdaftar(
                 "Username yang Anda masukkan tidak terdaftar! Mohon gunakan username lain."
             )
         elif res.url.endswith("#PasswordSalah"):
             raise PasswordSalah("Password yang Anda masukkan salah!")
+        res = self._get(res.headers["Location"])
         soup = BeautifulSoup(res.text, "html.parser")
         return Pengguna.from_soup(soup, self.__server)
+
+    def login_pengguna(self, pengguna: Pengguna) -> bool:
+        """Login dengan Pengguna
+
+        Args:
+            pengguna (Pengguna): Pengguna dari .login()
+
+        Returns:
+            bool: Berhasil / tidaknya login
+        """
+        return self._get(pengguna.url).ok
