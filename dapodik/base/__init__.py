fd2574f00b3bfed13c43ea89bdd0b9362fd94f87
hexatester <revolusi147id@gmail.com>
2021-01-15 23:15:38 +0700

    Refactor .base

--- dapodik/base/__init__.py
+++ dapodik/base/__init__.py
@@ -1,8 +1,6 @@
 from .attr import asdict, dataclass, sdataclass, field, fields
 from .utils import from_dict, from_list
 from .dapodik import BaseDapodik, DapodikChild
-from .results import Results
-from .types import TResult
 
 __all__ = [
     "asdict",
@@ -14,6 +12,4 @@ __all__ = [
     "from_list",
     "BaseDapodik",
     "DapodikChild",
-    "Results",
-    "TResult",
 ]
--- dapodik/base/types.py
+++
@@ -1,5 +0,0 @@
-from typing import MutableMapping, TypeVar, Union
-
-T = TypeVar("T")
-
-TResult = MutableMapping[Union[int, str], T]
