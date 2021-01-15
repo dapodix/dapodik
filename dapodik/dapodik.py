d610a5482513d2ad55c307ad68b44b4aa86e86ea
hexatester <revolusi147id@gmail.com>
2021-01-15 19:15:40 +0700

    Improve auth logic of Dapodik

--- dapodik/dapodik.py
+++ dapodik/dapodik.py
@@ -1,16 +1,35 @@
 from requests import Session
 
+from dapodik import __semester__
 from dapodik.base import BaseDapodik
+from dapodik.utils.decorator import lazy
+
 from dapodik.auth import BaseAuth
 
 
 class Dapodik(BaseDapodik):
     def __init__(
         self,
-        server="http://localhost:5774/",
-        session=Session(),
+        username: str,
+        password: str,
+        semester_id: str = __semester__,
+        server: str = "http://localhost:5774/",
+        session: Session = Session(),
         login: bool = True,
-        pengguna: int = 1,
+        pengguna: int = 0,
     ):
-        super().__init__(server=server, session=session)
-        self.auth = BaseAuth(self)
+        super(Dapodik, self).__init__(server=server, session=session)
+        self.__username = username
+        self.__password = password
+        self.__semester_id = semester_id
+        if login:
+            daftar_pengguna = self.auth.login(username, password, semester_id)
+            self.auth.login_pengguna(daftar_pengguna[pengguna])
+
+    @property  # type: ignore
+    @lazy
+    def auth(self) -> BaseAuth:
+        return BaseAuth(self)
+
+    def __del__(self):
+        self.auth.logout()
