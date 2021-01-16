36fb91e60cef9ef0ed41d01fa52e547c014e4afe
hexatester <revolusi147id@gmail.com>
2021-01-16 21:43:50 +0700

    Update docstring

--- dapodik/base/config.py
+++ dapodik/base/config.py
@@ -12,6 +12,17 @@ HEADERS = {
 
 @dataclass
 class Config:
+    f"""Config
+
+    Args:
+        username (str): Email dapodik
+        password (str): Password dapodik
+        server (str): Url server dapodik, default: http://localhost:5774/
+        semester_id (str): ID Semester, default: {__semester__}
+        sekolah_id (Optional[str]): ID Sekolah, default: None
+        session (Session): Sesi
+    """
+
     username: str
     password: str
     server: str = field(default="http://localhost:5774/")
