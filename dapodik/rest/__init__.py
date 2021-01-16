73b3ca322749086451d8c32ebbe74cb1d53b6c9a
hexatester <revolusi147id@gmail.com>
2021-01-16 21:17:49 +0700

    Added .rest.__all__

---
+++ dapodik/rest/__init__.py
@@ -0,0 +1,95 @@
+from .agama import Agama
+from .akreditasi import Akreditasi
+from .akses_internet import AksesInternet
+from .alat_transportasi import AlatTransportasi
+from .bank import Bank
+from .bentuk_lembaga import BentukLembaga
+from .biblio import Biblio
+from .child_delete import ChildDelete
+from .fasilitas_layanan import FasilitasLayanan
+from .gmd import Gmd
+from .jadwal_paud import JadwalPaud
+from .jenis_gugus import JenisGugus
+from .jenis_hapus_buku import JenisHapusBuku
+from .jenis_keluar import JenisKeluar
+from .jenis_lk import JenisLk
+from .jenis_pendaftaran import JenisPendaftaran
+from .jenis_prasarana import JenisPrasarana
+from .jenis_ptk import JenisPtk
+from .jenis_rombel import JenisRombel
+from .jenis_sarana import JenisSarana
+from .jenis_tinggal import JenisTinggal
+from .jenjang_pendidikan import JenjangPendidikan
+from .jurusan import Jurusan
+from .kategori_tk import KategoriTk
+from .keahlian_laboratorium import KeahlianLaboratorium
+from .kebutuhan_khusus import KebutuhanKhusus
+from .klasifikasi_lembaga import KlasifikasiLembaga
+from .kurikulum import Kurikulum
+from .lembaga_pengangkat import LembagaPengangkat
+from .mata_pelajaran import MataPelajaran
+from .mst_wilayah import MstWilayah
+from .pangkat_golongan import PangkatGolongan
+from .pekerjaan import Pekerjaan
+from .penghasilan import Penghasilan
+from .role_pengguna import RolePengguna
+from .status_keaktifan_pegawai import StatusKeaktifanPegawai
+from .status_kepegawaian import StatusKepegawaian
+from .status_kepemilikan_sarpras import StatusKepemilikanSarpras
+from .sumber_air import SumberAir
+from .sumber_dana_sekolah import SumberDanaSekolah
+from .sumber_gaji import SumberGaji
+from .sumber_listrik import SumberListrik
+from .sync_log import SyncLog
+from .tahun_ajaran import TahunAjaran
+from .tingkat_pendidikan import TingkatPendidikan
+from .waktu_penyelenggaraan import WaktuPenyelenggaraan
+
+__all__ = [
+    "Agama",
+    "Akreditasi",
+    "AksesInternet",
+    "AlatTransportasi",
+    "Bank",
+    "BentukLembaga",
+    "Biblio",
+    "ChildDelete",
+    "FasilitasLayanan",
+    "Gmd",
+    "JadwalPaud",
+    "JenisGugus",
+    "JenisHapusBuku",
+    "JenisKeluar",
+    "JenisLk",
+    "JenisPendaftaran",
+    "JenisPrasarana",
+    "JenisPtk",
+    "JenisRombel",
+    "JenisSarana",
+    "JenisTinggal",
+    "JenjangPendidikan",
+    "Jurusan",
+    "KategoriTk",
+    "KeahlianLaboratorium",
+    "KebutuhanKhusus",
+    "KlasifikasiLembaga",
+    "Kurikulum",
+    "LembagaPengangkat",
+    "MataPelajaran",
+    "MstWilayah",
+    "PangkatGolongan",
+    "Pekerjaan",
+    "Penghasilan",
+    "RolePengguna",
+    "StatusKeaktifanPegawai",
+    "StatusKepegawaian",
+    "StatusKepemilikanSarpras",
+    "SumberAir",
+    "SumberDanaSekolah",
+    "SumberGaji",
+    "SumberListrik",
+    "SyncLog",
+    "TahunAjaran",
+    "TingkatPendidikan",
+    "WaktuPenyelenggaraan",
+]
