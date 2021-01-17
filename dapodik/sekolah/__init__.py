c497b7b43b02a0c79379e0df92d6e9d2f54e37c7
hexatester <revolusi147id@gmail.com>
2021-01-17 10:13:01 +0700

    Added .sekolah.__all__

---
+++ dapodik/sekolah/__init__.py
@@ -0,0 +1,25 @@
+from .akreditasi_sp import AkreditasiSp
+from .blockgrant import BlockGrant
+from .jurusan_sp import JurusanSp
+from .kepanitiaan import Kepanitiaan
+from .program_inklusi import ProgramInklusi
+from .sanitasi import Sanitasi
+from .sekolah_longitudinal import SekolahLongitudinal
+from .sekolah_paud import SekolahPaud
+from .sekolah import Sekolah
+from .semester import Semester
+from .yayasan import Yayasan
+
+__all__ = [
+    "AkreditasiSp",
+    "BlockGrant",
+    "JurusanSp",
+    "Kepanitiaan",
+    "ProgramInklusi",
+    "Sanitasi",
+    "SekolahLongitudinal",
+    "SekolahPaud",
+    "Sekolah",
+    "Semester",
+    "Yayasan",
+]
