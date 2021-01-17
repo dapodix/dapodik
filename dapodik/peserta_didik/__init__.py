57a5ad8e23c889e2d8463975ab33fcb9d6d6ef94
hexatester <revolusi147id@gmail.com>
2021-01-17 19:02:32 +0700

    Added .peserta_didik.BasePesertaDidik

--- dapodik/peserta_didik/__init__.py
+++ dapodik/peserta_didik/__init__.py
@@ -2,10 +2,12 @@ from .peserta_didik_baru import PesertaDidikBaru
 from .peserta_didik_longitudinal import PesertaDidikLongitudinal
 from .peserta_didik import PesertaDidik
 from .registrasi_peserta_didik import RegistrasiPesertaDidik
+from .base import BasePesertaDidik
 
 __all__ = [
     "PesertaDidikBaru",
     "PesertaDidikLongitudinal",
     "PesertaDidik",
     "RegistrasiPesertaDidik",
+    "BasePesertaDidik",
 ]
---
+++ dapodik/peserta_didik/base.py
@@ -0,0 +1,64 @@
+from typing import List
+
+from dapodik.base import BaseDapodik
+from . import (
+    PesertaDidikBaru,
+    PesertaDidikLongitudinal,
+    PesertaDidik,
+    RegistrasiPesertaDidik,
+)
+
+__fetch__ = (
+    PesertaDidikBaru,
+    PesertaDidikLongitudinal,
+    PesertaDidik,
+    RegistrasiPesertaDidik,
+)
+
+
+class BasePesertaDidik(BaseDapodik):
+    __all__ = __fetch__
+
+    def peserta_didik_baru(self) -> List[PesertaDidikBaru]:
+        res = self._get_rest("PesertaDidikBaru")
+        data: dict = res.json()
+        return self._fl(PesertaDidikBaru, data.get("rows"))
+
+    def peserta_didik_longitudinal(self) -> List[PesertaDidikLongitudinal]:
+        res = self._get_rest("PesertaDidikLongitudinal")
+        data: dict = res.json()
+        return self._fl(PesertaDidikLongitudinal, data.get("rows"))
+
+    def peserta_didik(
+        self,
+        sekolah_id: str,
+        pd_module: str = "pdterdaftar",
+        page: int = 1,
+        start: int = 0,
+        limit: int = 25,
+    ) -> List[PesertaDidik]:
+        params = {
+            "sekolah_id": sekolah_id,
+            "pd_module": pd_module,
+            "page": page,
+            "start": start,
+            "limit": limit,
+        }
+        res = self._get_rest("PesertaDidik", params)
+        data: dict = res.json()
+        return self._fl(PesertaDidik, data.get("rows"))
+
+    def peserta_didik_keluar(
+        self,
+        sekolah_id: str,
+        pd_module: str = "pdkeluar",
+        page: int = 1,
+        start: int = 0,
+        limit: int = 25,
+    ) -> List[PesertaDidik]:
+        return self.peserta_didik(sekolah_id, pd_module, page, start, limit)
+
+    def registrasi_peserta_didik(self) -> List[RegistrasiPesertaDidik]:
+        res = self._get_rest("RegistrasiPesertaDidik")
+        data: dict = res.json()
+        return self._fl(RegistrasiPesertaDidik, data.get("rows"))
