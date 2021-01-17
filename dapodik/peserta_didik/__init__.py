074686688c3ee69f2c3d3eb9fa8fc5838b3a4b9a
hexatester <revolusi147id@gmail.com>
2021-01-17 10:15:13 +0700

    Added .peserta_didik.__all__

---
+++ dapodik/peserta_didik/__init__.py
@@ -0,0 +1,11 @@
+from .peserta_didik_baru import PesertaDidikBaru
+from .peserta_didik_longitudinal import PesertaDidikLongitudinal
+from .peserta_didik import PesertaDidik
+from .registrasi_peserta_didik import RegistrasiPesertaDidik
+
+__all__ = [
+    "PesertaDidikBaru",
+    "PesertaDidikLongitudinal",
+    "PesertaDidik",
+    "RegistrasiPesertaDidik",
+]
