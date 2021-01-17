19e0640200ffc7ad3a62b861aa96d6038d67012f
hexatester <revolusi147id@gmail.com>
2021-01-17 11:11:29 +0700

    Added BaseSekolah.sekolah

--- dapodik/sekolah/base.py
+++ dapodik/sekolah/base.py
@@ -1,5 +1,13 @@
 from dapodik.base import BaseDapodik
+from . import Sekolah
 
 
 class BaseSekolah(BaseDapodik):
-    pass
+    def __call__(self) -> Sekolah:
+        return self.sekolah()
+
+    def sekolah(self) -> Sekolah:
+        res = self._get_rest("Sekolah")
+        data: dict = res.json()
+        _sekolah: list = data.get("rows", list())
+        return self._fd(Sekolah, _sekolah[0])
