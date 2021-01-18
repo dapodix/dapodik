d93ce4a93cc4802b10b59a4eaf9897c1012b320e
hexatester <revolusi147id@gmail.com>
2021-01-18 09:31:07 +0700

    Add docstring for Defaults

--- dapodik/base/defaults.py
+++ dapodik/base/defaults.py
@@ -3,6 +3,14 @@ from . import dataclass
 
 @dataclass
 class Defaults:
+    """Defaults
+
+    Args:
+        sekolah_id (str): ID sekolah. Default ""
+        page (int): Halaman data yang diambil. Default 1
+        start (int): Dimulai dari. Default 0
+        limit (int): Batas data yang diambil, default 25
+    """
     sekolah_id: str = ""
     page: int = 1
     start: int = 0
