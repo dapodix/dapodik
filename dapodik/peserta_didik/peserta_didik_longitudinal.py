e8ec54953613d59e33fccdfeff77b5e2badea4c3
hexatester <revolusi147id@gmail.com>
2021-01-18 19:59:31 +0700

    Improve .peserta_didik.PesertaDidikLongitudinal

--- dapodik/peserta_didik/peserta_didik_longitudinal.py
+++ dapodik/peserta_didik/peserta_didik_longitudinal.py
@@ -1,22 +1,31 @@
-from dapodik.base import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from dapodik import __semester__
+from dapodik.base import dataclass, freeze
 
 DEF = "Admin.model.PesertaDidikLongitudinal-1"
 
 
 @dataclass
 class PesertaDidikLongitudinal:
-    peserta_didik_id: int
+    peserta_didik_id: UUID = freeze(default=None)
+    semester_id: str = freeze(default=__semester__)
     tinggi_badan: int
     berat_badan: int
-    lingkar_kepala: int = 0
-    peserta_didik_longitudinal_id: int = DEF  # type: ignore
-    semester_id: str = "20201"
-    jarak_rumah_ke_sekolah_km: int = 1
-    jarak_rumah_ke_sekolah: int = 1
-    waktu_tempuh_ke_sekolah: int = 0
-    menit_tempuh_ke_sekolah: int = 1
+    lingkar_kepala: int
+    jarak_rumah_ke_sekolah: int
+    jarak_rumah_ke_sekolah_km: int
+    waktu_tempuh_ke_sekolah: int
+    menit_tempuh_ke_sekolah: int
     jumlah_saudara_kandung: int = 0
-    vld_count: int = 0
-    peserta_didik_longitudinal_id_str: str = ""
-    peserta_didik_id_str: str = ""
-    semester_id_str: str = ""
+    create_date: datetime = freeze(default=None)
+    last_update: datetime = freeze(default=None)
+    soft_delete: int = freeze(default=None)
+    last_sync: datetime = freeze(default=None)
+    updater_id: UUID = freeze(default=None)
+    peserta_didik_longitudinal_id_str: str = freeze(default=None)
+    peserta_didik_id_str: str = freeze(default=None)
+    semester_id_str: int = freeze(default=None)
+    peserta_didik_longitudinal_id: str = freeze(default=DEF)
+    vld_count: int = freeze(default=None)
