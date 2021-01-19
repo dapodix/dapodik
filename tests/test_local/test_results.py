380214d1850debdd79f0bc07a047cab0fd50687b
hexatester <revolusi147id@gmail.com>
2021-01-19 11:05:55 +0700

    Add test_results

---
+++ tests/test_local/test_results.py
@@ -0,0 +1,14 @@
+from random import randint
+
+from dapodik import Dapodik
+from dapodik.base import Results
+
+
+def test_results(dapodik: Dapodik):
+    limit = randint(1, 8)
+    agama_agama = dapodik.rest.agama(limit=limit)
+    assert isinstance(agama_agama, Results)
+    if agama_agama.results > limit:
+        assert len(agama_agama) == limit
+    else:
+        assert len(agama_agama) <= limit
