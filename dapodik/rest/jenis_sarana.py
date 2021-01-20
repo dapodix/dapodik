e2b40c8ff438d7f8c84543328f113642126dbd9a
hexatester <revolusi147id@gmail.com>
2021-01-20 20:17:25 +0700

    Update JenisSarana

--- dapodik/rest/jenis_sarana.py
+++ dapodik/rest/jenis_sarana.py
@@ -7,10 +7,14 @@ from dapodik.base import dataclass
 @dataclass(frozen=True, slots=True)
 class JenisSarana:
     jenis_sarana_id: str
-    id_hapus_buku: str
-    ket_hapus_buku: str
-    u_prasarana: str
-    u_sarana: str
+    nama: str
+    kelompok: Optional[str]
+    perlu_penempatan: int
+    keterangan: str
+    a_alat: int
+    a_angkutan: int
+    spm_qty_min_per_siswa: str
+    spm_qty_min_per_sekolah: int
     create_date: datetime
     last_update: datetime
     expired_date: Optional[datetime]
