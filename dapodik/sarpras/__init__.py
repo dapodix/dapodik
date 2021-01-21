e9a4623835d140ab393b83ca6a7ad9ef14eb62e8
hexatester <revolusi147id@gmail.com>
2021-01-21 19:18:50 +0700

    Add .sarpras.BangunanLongitudinal

--- dapodik/sarpras/__init__.py
+++ dapodik/sarpras/__init__.py
@@ -4,6 +4,7 @@ from .alat import Alat
 from .angkutan_dari_blockgrant import AngkutanDariBlockgrant
 from .angkutan import Angkutan
 from .bangunan_dari_blockgrant import BangunanDariBlockgrant
+from .bangunan_longitudinal import BangunanLongitudinal
 
 __all__ = [
     "AlatDariBlockgrant",
@@ -12,4 +13,5 @@ __all__ = [
     "AngkutanDariBlockgrant",
     "Angkutan",
     "BangunanDariBlockgrant",
+    "BangunanLongitudinal",
 ]
---
+++ dapodik/sarpras/bangunan_longitudinal.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from dapodik.base import dataclass, freeze
+
+
+@dataclass
+class BangunanLongitudinal:
+    id_bangunan: str
+    semester_id: str
+    rusak_pondasi: str
+    ket_pondasi: str
+    rusak_sloop_kolom_balok: str
+    ket_sloop_kolom_balok: str
+    rusak_plester_struktur: str
+    ket_plester_struktur: str
+    rusak_kudakuda_atap: str
+    ket_kudakuda_atap: str
+    rusak_kaso_atap: str
+    ket_kaso_atap: str
+    rusak_reng_atap: str
+    ket_reng_atap: str
+    rusak_tutup_atap: str
+    ket_tutup_atap: str
+    nilai_saat_ini: str
+    id_bangunan_str: str
+    semester_id_str: str
+    bangunan_longitudinal_id: str
+    create_date: datetime = freeze(default=None)
+    last_update: datetime = freeze(default=None)
+    soft_delete: str = freeze(default=None)
+    last_sync: datetime = freeze(default=None)
+    updater_id: str = freeze(default=None)
