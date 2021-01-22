e008b17224b5c5a37cf945dad11c35c0e029a9c4
hexatester <revolusi147id@gmail.com>
2021-01-22 23:01:17 +0700

    Add AlatLongitudinal.alat_longitudinal_id_str

--- dapodik/sarpras/alat_longitudinal.py
+++ dapodik/sarpras/alat_longitudinal.py
@@ -12,6 +12,7 @@ class AlatLongitudinal:
     id_alat_str: str
     semester_id_str: str
     alat_longitudinal_id: str
+    alat_longitudinal_id_str: str = ""
     create_date: datetime = freeze(default=None)
     last_update: datetime = freeze(default=None)
     soft_delete: str = freeze(default=None)
