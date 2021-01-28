0fc254b29f1fee2219f1d9087597cef61d5546b3
hexatester <revolusi147id@gmail.com>
2021-01-28 13:47:49 +0700

    Update serializer

--- dapodik/base/attrs/serializer.py
+++ dapodik/base/attrs/serializer.py
@@ -9,31 +9,10 @@ def serializer(
 ) -> Any:
     if not value:
         return value
-    if not name:
-        name = getattr(field, "name", "")
     if isinstance(value, list):
-        out = {}
-        for idx, val in enumerate(value):
-            val = serializer(value=val)
-            if isinstance(val, dict):
-                for key, value in val.items():
-                    out[f"{name}[{idx}][{key}]"] = val[key]
-            else:
-                out_key = name or ""
-                # Check if data required name prefix
-                if hasattr(value, "name"):
-                    out_key += f"[{getattr(value, 'name')}]"
-                out_key += f"[{idx}]"
-                out[out_key] = val
-        return out
+        return value
     elif isinstance(value, dict):
-        out = {}
-        for key, value in value.items():
-            if isinstance(value, list):
-                out.update(serializer(value=val, name=key))
-            else:
-                out[key] = value
-        return out
+        return value
     elif attr.has(value):
         return attr.asdict(value, value_serializer=serializer)  # type: ignore
     elif isinstance(value, UUID):
