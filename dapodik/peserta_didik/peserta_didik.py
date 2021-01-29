89f7e8d007ecc6ef17f62ffa237aa99f9a86f13d
hexatester <revolusi147id@gmail.com>
2021-01-29 13:47:31 +0700

    Add PesertaDidik.Create

--- dapodik/peserta_didik/peserta_didik.py
+++ dapodik/peserta_didik/peserta_didik.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date
-from typing import Optional
+from typing import List, Optional
 from uuid import UUID
 from dapodik.base import dataclass, field, freeze
 
@@ -105,3 +105,91 @@ class PesertaDidik:
 
     def __str__(self):
         return self.nama
+
+    @dataclass
+    class Create:
+        nama: str
+        jenis_kelamin: str
+        nisn: str = ""
+        nik: str
+        tempat_lahir: str
+        tanggal_lahir: datetime
+        nama_ibu_kandung: str
+        sekolah_id: UUID
+        pdb_id: UUID
+        kewarganegaraan: str
+        tahun_lahir_ayah: int
+        tahun_lahir_ibu: int
+        tahun_lahir_wali: int
+        vld_count: int = 0
+        peserta_didik_id: str = "Admin.model.PesertaDidik-1"
+        kewarganegaraan_str: str
+        no_kk: str
+        reg_akta_lahir: str = ""
+        agama_id: int
+        agama_id_str: str = ""
+        kebutuhan_khusus_id: int = 0
+        kebutuhan_khusus_id_str: str = ""
+        alamat_jalan: str
+        rt: int = 0
+        rw: int = 0
+        nama_dusun: str
+        desa_kelurahan: str
+        kode_wilayah: str
+        kode_wilayah_str: str
+        kode_pos: int
+        lintang: int
+        bujur: int
+        jenis_tinggal_id: int
+        jenis_tinggal_id_str: str
+        alat_transportasi_id: int
+        alat_transportasi_id_str: str = ""
+        no_kks: str = ""
+        anak_keberapa: int = 1
+        penerima_kps: int = 0
+        no_kps: str = ""
+        penerima_kip: int = 0
+        no_kip: str = ""
+        layak_pip: int = 0
+        id_layak_pip: Optional[int] = None
+        id_layak_pip_str: str
+        id_bank: str = ""
+        id_bank_str: str = ""
+        rekening_bank: str = ""
+        nama_kcp: str = ""
+        rekening_atas_nama: str = ""
+        status_data: int = 0
+        nama_ayah: str
+        nik_ayah: str
+        jenjang_pendidikan_ayah: int
+        jenjang_pendidikan_ayah_str: str
+        pekerjaan_id_ayah: int
+        pekerjaan_id_ayah_str: str
+        penghasilan_id_ayah: int
+        penghasilan_id_ayah_str: str
+        kebutuhan_khusus_id_ayah: int = 0
+        kebutuhan_khusus_id_ayah_str: str = ""
+        nik_ibu: str
+        jenjang_pendidikan_ibu: int
+        jenjang_pendidikan_ibu_str: str = ""
+        pekerjaan_id_ibu: int
+        pekerjaan_id_ibu_str: str = ""
+        penghasilan_id_ibu: int
+        penghasilan_id_ibu_str: str = ""
+        kebutuhan_khusus_id_ibu: int = 0
+        kebutuhan_khusus_id_ibu_str: str = ""
+        nm_kip: int = 0
+        nama_wali: str = ""
+        nik_wali: str = ""
+        jenjang_pendidikan_wali: int = 0
+        jenjang_pendidikan_wali_str: str = ""
+        pekerjaan_id_wali: int = 0
+        pekerjaan_id_wali_str: str = ""
+        penghasilan_id_wali: int = 0
+        penghasilan_id_wali_str: str = ""
+        nomor_telepon_rumah: str = ""
+        nomor_telepon_seluler: str = ""
+        email: str = ""
+        kebutuhan_khusus_id_selector: List[int] = field(factory=list)
+        kebutuhan_khusus_id_selector_ayah: List[int] = field(factory=list)
+        kebutuhan_khusus_id_selector_ibu: List[int] = field(factory=list)
