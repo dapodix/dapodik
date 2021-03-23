c85cfe6be06bec60094c6006c6a6d7340603b558
hexatester <revolusi147id@gmail.com>
2021-03-23 23:01:12 +0700

    Change test_dapodik to test_version

---
+++ tests/test_version.py
@@ -0,0 +1,18 @@
+from dapodik import __version__, __dapodik_version__, __semester__, __tahun_ajaran__
+
+
+def test_version():
+    assert __version__ == "0.12.0"
+
+
+def test_dapodik_version():
+    assert __dapodik_version__ == "2021.d"
+
+
+def test_tahun_ajaran():
+    assert __tahun_ajaran__ == "2020"
+
+
+def test_semester():
+    assert __semester__ == "20202"
+    assert __semester__.startswith(__tahun_ajaran__)
