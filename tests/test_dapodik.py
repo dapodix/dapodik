3ddccc01d9e13fd29aae61a81054c0c00932a71c
hexatester <revolusi147id@gmail.com>
2021-03-23 23:01:31 +0700

    Add test_dapodik

--- tests/test_dapodik.py
+++ tests/test_dapodik.py
@@ -1,18 +1,20 @@
-from dapodik import __version__, __dapodik_version__, __semester__, __tahun_ajaran__
+from dapodik import Dapodik
+from dapodik.base import BaseDapodik
+from dapodik.auth import BaseAuth
+from dapodik.peserta_didik import BasePesertaDidik
+from dapodik.rest import BaseRest
+from dapodik.rombongan_belajar import BaseRombonganBelajar
+from dapodik.sarpras import BaseSarpras
+from dapodik.sekolah import BaseSekolah
+from dapodik.validasi import BaseValidasi
 
 
-def test_version():
-    assert __version__ == "0.12.0"
-
-
-def test_dapodik_version():
-    assert __dapodik_version__ == "2021.d"
-
-
-def test_tahun_ajaran():
-    assert __tahun_ajaran__ == "2020"
-
-
-def test_semester():
-    assert __semester__ == "20202"
-    assert __semester__.startswith(__tahun_ajaran__)
+def test_dapodik():
+    assert issubclass(Dapodik, BaseDapodik)
+    assert issubclass(Dapodik, BaseAuth)
+    assert issubclass(Dapodik, BasePesertaDidik)
+    assert issubclass(Dapodik, BaseRest)
+    assert issubclass(Dapodik, BaseRombonganBelajar)
+    assert issubclass(Dapodik, BaseSarpras)
+    assert issubclass(Dapodik, BaseSekolah)
+    assert issubclass(Dapodik, BaseValidasi)
