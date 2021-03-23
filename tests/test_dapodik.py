29bcb0c37dc130fb3041470505a948f88ca9b34e
hexatester <revolusi147id@gmail.com>
2021-03-23 22:56:56 +0700

    Refactor test_dapodik

--- tests/test_dapodik.py
+++ tests/test_dapodik.py
@@ -3,7 +3,16 @@ from dapodik import __version__, __dapodik_version__, __semester__, __tahun_ajar
 
 def test_version():
     assert __version__ == "0.12.0"
+
+
+def test_dapodik_version():
     assert __dapodik_version__ == "2021.d"
-    assert __semester__ == "20202"
+
+
+def test_tahun_ajaran():
     assert __tahun_ajaran__ == "2020"
+
+
+def test_semester():
+    assert __semester__ == "20202"
     assert __semester__.startswith(__tahun_ajaran__)
