c008afacc3612a73963e7f9cf84aea900e3b6ada
hexatester <revolusi147id@gmail.com>
2021-03-24 14:33:45 +0700

    Add JenisKelamin.__str__

--- dapodik/enums.py
+++ dapodik/enums.py
@@ -4,3 +4,10 @@ from enum import Enum
 class JenisKelamin(Enum):
     LAKI_LAKI = "L"
     PEREMPUAN = "P"
+
+    def __str__(self) -> str:
+        if self.value == self.LAKI_LAKI:
+            return "Laki-laki"
+        elif self.value == self.PEREMPUAN:
+            return "Perempuan"
+        return "Tidak terdefinisi"
