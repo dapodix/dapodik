95f7a6593363feb8ab3f676f2c45d3fe35b35aec
hexatester <revolusi147id@gmail.com>
2021-03-24 14:00:22 +0700

    Initial dapodik.ptk.enums

---
+++ dapodik/gtk/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class PtkInduk(Enum):
+    INDUK = "1"
+    NON_INDUK = "0"
+
+    def __bool__(self):
+        return self.value == 1
+
+
+class PtkAktif(Enum):
+    AKTIF = "1"
+    NON_AKTIF = "0"
+
+    def __bool__(self):
+        return self.value == 1
