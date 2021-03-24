ee18b3d085bde87b1311da8c87cb47d265a5b935
hexatester <revolusi147id@gmail.com>
2021-03-24 13:15:30 +0700

    Update PtkInduk and PtkAktif

--- dapodik/gtk/ptk.py
+++ dapodik/gtk/ptk.py
@@ -6,16 +6,16 @@ from uuid import UUID
 
 
 class PtkInduk(Enum):
-    INDUK = 1
-    NON_INDUK = 0
+    INDUK = "1"
+    NON_INDUK = "0"
 
     def __bool__(self):
         return self.value == 1
 
 
 class PtkAktif(Enum):
-    AKTIF = 1
-    NON_AKTIF = 0
+    AKTIF = "1"
+    NON_AKTIF = "0"
 
     def __bool__(self):
         return self.value == 1
