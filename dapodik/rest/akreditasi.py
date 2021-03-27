d2360c1fde6de148e44dd7deb014356c4cc77f1d
hexatester <revolusi147id@gmail.com>
2021-03-27 18:24:26 +0700

    Added Akreditasi.Prop

--- dapodik/rest/akreditasi.py
+++ dapodik/rest/akreditasi.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -15,3 +16,19 @@ class Akreditasi:
 
     def __str__(self):
         return self.nama
+
+    class Prop(BaseProp):
+        @property
+        def akreditasi(self) -> "Akreditasi":
+            return self.dapodik._find(
+                self.dapodik.akreditasi(),
+                lambda x: x.akreditasi_id == getattr(self, "akreditasi_id"),
+            )
+
+        @akreditasi.setter
+        def akreditasi(self, value: "Akreditasi"):
+            new_akreditasi = self.dapodik._find(
+                self.dapodik.akreditasi(),
+                lambda x: x.akreditasi_id == value.akreditasi_id,
+            )
+            setattr(self, "akreditasi_id", new_akreditasi.akreditasi_id)
