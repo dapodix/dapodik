39da5943212cd4d024d56cb0be522503cd784f00
hexatester <revolusi147id@gmail.com>
2021-03-27 19:47:26 +0700

    Added JenjangPendidikan.Prop

--- dapodik/rest/jenjang_pendidikan.py
+++ dapodik/rest/jenjang_pendidikan.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -17,3 +18,20 @@ class JenjangPendidikan:
 
     def __str__(self):
         return self.nama
+
+    class Prop(BaseProp):
+        @property
+        def jenjang_pendidikan(self) -> "JenjangPendidikan":
+            return self.dapodik._find(
+                self.dapodik.jenjang_pendidikan(),
+                lambda x: x.jenjang_pendidikan_id
+                == getattr(self, "jenjang_pendidikan_id"),
+            )
+
+        @jenjang_pendidikan.setter
+        def jenjang_pendidikan(self, value: "JenjangPendidikan"):
+            new = self.dapodik._find(
+                self.dapodik.jenjang_pendidikan(),
+                lambda x: x.jenjang_pendidikan_id == value.jenjang_pendidikan_id,
+            )
+            setattr(self, "jenjang_pendidikan_id", new.jenjang_pendidikan_id)
