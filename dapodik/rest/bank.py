6b67cba9823a2a88db0915796cd294829c0495f7
hexatester <revolusi147id@gmail.com>
2021-03-27 18:35:48 +0700

    Added Bank.Prop

--- dapodik/rest/bank.py
+++ dapodik/rest/bank.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -15,3 +16,19 @@ class Bank:
 
     def __str__(self):
         return self.nm_bank
+
+    class Prop(BaseProp):
+        @property
+        def bank(self) -> "Bank":
+            return self.dapodik._find(
+                self.dapodik.bank(),
+                lambda x: x.id_bank == getattr(self, "id_bank"),
+            )
+
+        @bank.setter
+        def bank(self, value: "Bank"):
+            new_bank = self.dapodik._find(
+                self.dapodik.bank(),
+                lambda x: x.id_bank == value.id_bank,
+            )
+            setattr(self, "id_bank", new_bank.id_bank)
