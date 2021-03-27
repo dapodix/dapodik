f9ffb173c7f0f4410b8f761c3f2d4fb9903498dc
hexatester <revolusi147id@gmail.com>
2021-03-27 19:43:11 +0700

    Added JenisSarana.Prop

--- dapodik/rest/jenis_sarana.py
+++ dapodik/rest/jenis_sarana.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -19,3 +20,19 @@ class JenisSarana:
     last_update: datetime
     expired_date: Optional[datetime]
     last_sync: datetime
+
+    class Prop(BaseProp):
+        @property
+        def jenis_sarana(self) -> "JenisSarana":
+            return self.dapodik._find(
+                self.dapodik.jenis_sarana(),
+                lambda x: x.jenis_sarana_id == getattr(self, "jenis_sarana_id"),
+            )
+
+        @jenis_sarana.setter
+        def jenis_sarana(self, value: "JenisSarana"):
+            new = self.dapodik._find(
+                self.dapodik.jenis_sarana(),
+                lambda x: x.jenis_sarana_id == value.jenis_sarana_id,
+            )
+            setattr(self, "jenis_sarana_id", new.jenis_sarana_id)
