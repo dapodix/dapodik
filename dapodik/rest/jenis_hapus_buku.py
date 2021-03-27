21dfc607fc2add91f8d5efbd6c450a71f7337f09
hexatester <revolusi147id@gmail.com>
2021-03-27 19:18:32 +0700

    Added JenisHapusBuku.Prop

--- dapodik/rest/jenis_hapus_buku.py
+++ dapodik/rest/jenis_hapus_buku.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -14,3 +15,19 @@ class JenisHapusBuku:
     last_update: datetime
     expired_date: Optional[datetime]
     last_sync: datetime
+
+    class Prop(BaseProp):
+        @property
+        def jenis_hapus_buku(self) -> "JenisHapusBuku":
+            return self.dapodik._find(
+                self.dapodik.jenis_hapus_buku(),
+                lambda x: x.id_hapus_buku == getattr(self, "id_hapus_buku"),
+            )
+
+        @jenis_hapus_buku.setter
+        def jenis_hapus_buku(self, value: "JenisHapusBuku"):
+            new_hapus_buku = self.dapodik._find(
+                self.dapodik.jenis_hapus_buku(),
+                lambda x: x.id_hapus_buku == value.id_hapus_buku,
+            )
+            setattr(self, "id_hapus_buku", new_hapus_buku.id_hapus_buku)
