20146be6d0dbd6a3c614ad55b713270d939389e0
hexatester <revolusi147id@gmail.com>
2021-03-27 18:25:45 +0700

    Added AksesInternet.Prop

--- dapodik/rest/akses_internet.py
+++ dapodik/rest/akses_internet.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -16,3 +17,19 @@ class AksesInternet:
 
     def __str__(self):
         return self.nama
+
+    class Prop(BaseProp):
+        @property
+        def akses_internet(self) -> "AksesInternet":
+            return self.dapodik._find(
+                self.dapodik.akses_internet(),
+                lambda x: x.akses_internet_id == getattr(self, "akses_internet_id"),
+            )
+
+        @akses_internet.setter
+        def akses_internet(self, value: "AksesInternet"):
+            new_akses_internet = self.dapodik._find(
+                self.dapodik.akses_internet(),
+                lambda x: x.akses_internet_id == value.akses_internet_id,
+            )
+            setattr(self, "akses_internet_id", new_akses_internet.akses_internet_id)
