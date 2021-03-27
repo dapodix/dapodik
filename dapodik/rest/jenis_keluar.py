e3decc7817aa3e3f43fceb07774f5ecc3bc44e07
hexatester <revolusi147id@gmail.com>
2021-03-27 19:24:15 +0700

    Added JenisKeluar.Prop

--- dapodik/rest/jenis_keluar.py
+++ dapodik/rest/jenis_keluar.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -14,3 +15,22 @@ class JenisKeluar:
     last_update: datetime
     expired_date: Optional[datetime]
     last_sync: datetime
+
+    class Prop(BaseProp):
+        @property
+        def jenis_keluar(self) -> Optional["JenisKeluar"]:
+            try:
+                return self.dapodik._find(
+                    self.dapodik.jenis_keluar(),
+                    lambda x: x.jenis_keluar_id == getattr(self, "jenis_keluar_id"),
+                )
+            except ValueError:
+                return None
+
+        @jenis_keluar.setter
+        def jenis_keluar(self, value: "JenisKeluar"):
+            new_jenis_keluar = self.dapodik._find(
+                self.dapodik.jenis_keluar(),
+                lambda x: x.jenis_keluar_id == value.jenis_keluar_id,
+            )
+            setattr(self, "jenis_keluar_id", new_jenis_keluar.jenis_keluar_id)
