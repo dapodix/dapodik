647cd4f84f6538f40439020fe9c977c5b1b906c6
hexatester <revolusi147id@gmail.com>
2021-03-27 19:29:55 +0700

    Added JenisPendaftaran.Prop

--- dapodik/rest/jenis_pendaftaran.py
+++ dapodik/rest/jenis_pendaftaran.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -17,3 +18,20 @@ class JenisPendaftaran:
 
     def __str__(self):
         return self.nama
+
+    class Prop(BaseProp):
+        @property
+        def jenis_pendaftaran(self) -> "JenisPendaftaran":
+            return self.dapodik._find(
+                self.dapodik.jenis_pendaftaran(),
+                lambda x: x.jenis_pendaftaran_id
+                == getattr(self, "jenis_pendaftaran_id"),
+            )
+
+        @jenis_pendaftaran.setter
+        def jenis_pendaftaran(self, value: "JenisPendaftaran"):
+            new = self.dapodik._find(
+                self.dapodik.jenis_pendaftaran(),
+                lambda x: x.jenis_pendaftaran_id == value.jenis_pendaftaran_id,
+            )
+            setattr(self, "jenis_pendaftaran_id", new.jenis_pendaftaran_id)
