e931d0c989552c8352375819e9ecda0d606ca813
hexatester <revolusi147id@gmail.com>
2021-03-27 19:32:13 +0700

    Added JenisPrasarana.Prop

--- dapodik/rest/jenis_prasarana.py
+++ dapodik/rest/jenis_prasarana.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -19,3 +20,19 @@ class JenisPrasarana:
 
     def __str__(self):
         return self.nama
+
+    class Prop(BaseProp):
+        @property
+        def jenis_prasarana(self) -> "JenisPrasarana":
+            return self.dapodik._find(
+                self.dapodik.jenis_prasarana(),
+                lambda x: x.jenis_prasarana_id == getattr(self, "jenis_prasarana_id"),
+            )
+
+        @jenis_prasarana.setter
+        def jenis_prasarana(self, value: "JenisPrasarana"):
+            new = self.dapodik._find(
+                self.dapodik.jenis_prasarana(),
+                lambda x: x.jenis_prasarana_id == value.jenis_prasarana_id,
+            )
+            setattr(self, "jenis_prasarana_id", new.jenis_prasarana_id)
