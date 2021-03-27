44e3bff7b998f5326bb7604618c5f44b29b8bcb3
hexatester <revolusi147id@gmail.com>
2021-03-27 19:50:30 +0700

    Added KategoriTk.Prop

--- dapodik/rest/kategori_tk.py
+++ dapodik/rest/kategori_tk.py
@@ -1,7 +1,8 @@
+import attr
 from datetime import datetime
 from typing import Optional
 
-import attr
+from dapodik.base import BaseProp
 
 
 @attr.dataclass(frozen=True, slots=True)
@@ -15,3 +16,19 @@ class KategoriTk:
 
     def __str__(self):
         return self.nama
+
+    class Prop(BaseProp):
+        @property
+        def kategori_tk(self) -> "KategoriTk":
+            return self.dapodik._find(
+                self.dapodik.kategori_tk(),
+                lambda x: x.kategori_tk_id == getattr(self, "kategori_tk_id"),
+            )
+
+        @kategori_tk.setter
+        def kategori_tk(self, value: "KategoriTk"):
+            new = self.dapodik._find(
+                self.dapodik.kategori_tk(),
+                lambda x: x.kategori_tk_id == value.kategori_tk_id,
+            )
+            setattr(self, "kategori_tk_id", new.kategori_tk_id)
