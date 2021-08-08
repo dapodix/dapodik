83546ef07bc1aa4edc0d7969edcb99f6afc8df72
hexatester <revolusi147id@gmail.com>
2021-08-08 20:52:23 +0700

    Added test_date_to_str and test_datetime_to_str

--- tests/test_utils/test_parser.py
+++ tests/test_utils/test_parser.py
@@ -1,5 +1,10 @@
 from datetime import date, datetime
-from dapodik.utils.parser import str_to_date, str_to_datetime
+from dapodik.utils.parser import (
+    str_to_date,
+    str_to_datetime,
+    date_to_str,
+    datetime_to_str,
+)
 
 
 def test_str_to_date():
@@ -21,3 +26,23 @@ def test_str_to_datetime():
     assert res.hour == 22
     assert res.minute == 59
     assert res.second == 30
+
+
+def lten(val: int) -> str:
+    if val < 10:
+        return "0" + str(val)
+    return str(val)
+
+
+def test_date_to_str():
+    now = datetime.now().date()
+    now_str = date_to_str(now)
+    assert now_str == f"{now.year}-{lten(now.month)}-{lten(now.day)}"
+
+
+def test_datetime_to_str():
+    now = datetime.now()
+    now_str = datetime_to_str(now)
+    now_str_ = f"{now.year}-{lten(now.month)}-{lten(now.day)} "
+    now_str_ += f"{lten(now.hour)}:{lten(now.minute)}:{lten(now.second)}"
+    assert now_str == now_str_
