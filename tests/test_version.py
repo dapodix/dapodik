f72b72e635dacf1408e5a1e7cb2aa8adccb67089
hexatester <revolusi147id@gmail.com>
2021-08-09 11:17:58 +0700

    Bumping version from 0.16.1 to 0.16.2

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,7 @@ from dapodik import __version__, __dapodik_version__, __semester__, __tahun_ajar
 
 
 def test_version():
-    assert __version__ == "0.16.1"
+    assert __version__ == "0.16.2"
 
 
 def test_dapodik_version():
