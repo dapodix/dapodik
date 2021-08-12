7ed436bab8b75c4cc6679c851599331e982acc2d
hexatester <revolusi147id@gmail.com>
2021-08-12 23:58:19 +0700

    Remove BaseProp

--- dapodik/base/__init__.py
+++ dapodik/base/__init__.py
@@ -1,6 +1,5 @@
 from .message import DapodikMessage
-from .base_prop import BaseProp
 from .base_dapodik import BaseDapodik
 
 
-__all__ = ["DapodikMessage", "BaseProp", "BaseDapodik"]
+__all__ = ["DapodikMessage", "BaseDapodik"]
