aa41ba1efcd6c579672e4b145af75a13ebe63ca1
hexatester <revolusi147id@gmail.com>
2021-08-27 02:33:39 +0700

    Improve DapodikMessage

--- dapodik/message.py
+++ dapodik/message.py
@@ -13,3 +13,19 @@ class DapodikMessage:
         obj = obj.replace("'", '"')
         data = json.loads(obj)
         return cattr.structure(data, cls)
+
+    @classmethod
+    def from_fail(cls, res: str) -> "DapodikMessage":
+        assert res.startswith("{ 'success' : false, 'message' : '")
+        res = res.replace(
+            "{ 'success' : false, 'message' : '", '{ "success" : false, "message" : "'
+        )
+        res = res.replace("' }", '" }')
+        data = json.loads(res)
+        return cattr.structure(data, cls)
+
+    def __str__(self):
+        return self.message
+
+    def __bool__(self):
+        return self.success
