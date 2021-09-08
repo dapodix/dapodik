e4b7b1b7a0370a35f8d39fb8dbf608efef5318f8
hexatester <revolusi147id@gmail.com>
2021-09-08 21:47:54 +0700

    Added .semester

--- dapodik/sekolah/base.py
+++ dapodik/sekolah/base.py
@@ -2,12 +2,16 @@ from dapodik.base import BaseDapodik
 from typing import List
 
 from . import Sekolah
+from . import Semester
 from . import Yayasan
 
 
 class BaseSekolah(BaseDapodik):
     def sekolah(self, index: int = 0) -> Sekolah:
         return self._get_rows("/rest/Sekolah", List[Sekolah])[index]
+
+    def semester(self) -> List[Semester]:
+        return self._get_rows("/rest/Semester", List[Semester])
 
     def yayasan(self) -> List[Yayasan]:
         return self._get_rows("/rest/Yayasan", List[Yayasan])
