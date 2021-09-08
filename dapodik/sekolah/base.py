36572b514b3e6d294a6e3df548a94f0d78edbc75
hexatester <revolusi147id@gmail.com>
2021-09-08 21:46:41 +0700

    Added .yayasan

--- dapodik/sekolah/base.py
+++ dapodik/sekolah/base.py
@@ -2,8 +2,12 @@ from dapodik.base import BaseDapodik
 from typing import List
 
 from . import Sekolah
+from . import Yayasan
 
 
 class BaseSekolah(BaseDapodik):
     def sekolah(self, index: int = 0) -> Sekolah:
         return self._get_rows("/rest/Sekolah", List[Sekolah])[index]
+
+    def yayasan(self) -> List[Yayasan]:
+        return self._get_rows("/rest/Yayasan", List[Yayasan])
