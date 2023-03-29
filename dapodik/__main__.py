aa9ebb4fc2bcace4f8f2d4a5e50bb4674ce027ee
hexatester <hexatester@protonmail.com>
2023-03-29 14:59:29 +0700

    Refactor main

--- dapodik/__main__.py
+++ dapodik/__main__.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
-from dapodik.main import main
+import sys
+from . import __version__, __dapodik_version__, __semester__, __tahun_ajaran__
+
+
+def print_ver():
+    print("python dapodik versi v{}".format(__version__))
+    print("Client / SDK python untuk dapodik versi {}".format(__dapodik_version__))
+    print("Semester {}".format(__semester__))
+    print("Tahun ajaran {}".format(__tahun_ajaran__))
+    print("Python {}".format(sys.version.replace("\n", " ")))
+
+
+def main():
+    sys.exit(print_ver())
+
 
 if __name__ == "__main__":
     main()
