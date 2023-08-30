69f467909af372ae4c30ca6f5c32d336f66dfc16
hexatester <hexatester@protonmail.com>
2023-08-31 01:02:34 +0700

    Bumping version from 0.18.1 to 0.19.0

--- dapodik/version.py
+++ dapodik/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = "0.18.1"
+__version__ = "0.19.0"
 __dapodik_version__ = "2023.a"
 __semester__ = "20231"
 __tahun_ajaran__ = "2023"
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,7 @@ from dapodik import __version__, __dapodik_version__, __semester__, __tahun_ajar
 
 
 def test_version():
-    assert __version__ == "0.18.1"
+    assert __version__ == "0.19.0"
 
 
 def test_dapodik_version():
